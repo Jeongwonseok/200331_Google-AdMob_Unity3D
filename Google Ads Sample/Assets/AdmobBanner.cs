a49c95a0e88bb99cf1c632f9ccc0e4d1382d204d
“Jeongwonseok” <jws4281@nate.com>
2020-03-31 17:48:44 +0900

    ㅅ

---
+++ Google Ads Sample/Assets/AdmobBanner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdmobBanner : MonoBehaviour
+{
+    private readonly string unitID = "ca-app-pub-3940256099942544/6300978111";
+    private readonly string test_unitID = "ca-app-pub-3940256099942544/6300978111";
+
+    private readonly string test_deviceID = "";
+
+    private BannerView banner;
+
+    public AdPosition position;
+
+    private void Start()
+    {
+        InitAd();
+    }
+
+    private void InitAd()
+    {
+        string id = Debug.isDebugBuild ? test_unitID : unitID;
+
+        banner = new BannerView(id, AdSize.SmartBanner, position);
+
+        //AdRequest request = new AdRequest.Builder().AddTestDevice(test_deviceID).Build();
+        AdRequest request = new AdRequest.Builder().Build();
+
+        banner.LoadAd(request);
+    }
+
+    public void ToggleAd(bool active)
+    {
+        if (active)
+        {
+            banner.Show();
+        }
+        else
+        {
+            banner.Hide();
+        }
+    }
+
+    public void DestroyAd()
+    {
+        banner.Destroy();
+    }
+}
---
+++ Google Ads Sample/Assets/AdmobScreenAd.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdmobScreenAd : MonoBehaviour
+{
+    private readonly string unitID = "ca-app-pub-3940256099942544/1033173712";
+    private readonly string test_unitID = "ca-app-pub-3940256099942544/1033173712";
+
+    private readonly string test_deviceID = "";
+
+    private InterstitialAd screedAd;
+
+    private void Start()
+    {
+        InitAd();
+        Invoke("Show", 10f);
+    }
+
+    private void InitAd()
+    {
+        string id = Debug.isDebugBuild ? test_unitID : unitID;
+
+        screedAd = new InterstitialAd(id);
+
+        AdRequest request = new AdRequest.Builder().Build();
+
+        screedAd.LoadAd(request);
+
+        screedAd.OnAdClosed += (sender, e) => Debug.Log("광고가 닫힘");
+        screedAd.OnAdLoaded += (sender, e) => Debug.Log("광고가 로드됨");
+    }
+
+    public void Show()
+    {
+        StartCoroutine(ShowScreedAd());
+    }
+
+    private IEnumerator ShowScreedAd()
+    {
+        while(!screedAd.IsLoaded())
+        {
+            yield return null;
+        }
+
+        screedAd.Show();
+    }
+}
